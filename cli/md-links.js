f8dda254e2b859b513395a0948b7c616dde7a260
Yennyfer Jarava <yennyfer.jarava92@gmail.com>
2023-08-17 22:39:59 -0500

    Installable module configuration

--- cli/md-links.js
+++ cli/md-links.js
@@ -1,24 +1,29 @@
+#!/usr/bin/env node
 const yargs = require("yargs");
 const { mdLinks, } = require('../index.js');
 const path = require('path');
-const colors = require('ansi-colors');
+const chalk = require('chalk');
+const boxen = require('boxen');
+
+console.log(boxen('¡Bienvenid@ a esta librería!', {padding: 0, margin: 0, borderStyle: 'double' }));
 
 const options = yargs(process.argv.slice(2))
-    .usage('md-links ./path/to/file.md -v -s')
-    .command('$0', 'Default command')
+    .usage(chalk.bold.green('md-links ./path/to/file.md -v -s'))
+    .command('$0', chalk.blue('Default command'))
     .option('v', {
         alias: 'validate',
-        describe: 'Verifica si el link funciona',
+        describe: chalk.yellow('Verifica si el link funciona'),
         type: 'boolean',
         demandOption: false,
     })
     .option("s", {
         alias: "stats",
-        describe: "Imprime las estadisticas de los links encontrados en cada archivo .md",
+        describe: chalk.cyan('Imprime las estadisticas de los links encontrados en cada archivo .md'),
         type: 'boolean',
         demandOption: false
     })
     .help(true)
+    .demandCommand()
     .argv;
 
 //calcula y muestra la cantidad total de enlaces únicos en el archivo Md.
@@ -48,29 +53,51 @@ if (route) {
         .then(links => {
             if (mdLinksOptions.stats) {
                 // Mostrar estadísticas
-                console.log(`Total: ${links.length}`);
-                console.log(`Unique: ${getUniqueLinksCount(links)}`);
+                const totalStats = chalk.bgMagenta.white(`Total: ${links.length}`);
+                const uniqueStats = chalk.bgYellowBright.black(`Unique: ${getUniqueLinksCount(links)}`);
+
+                console.log(totalStats);
+                console.log(uniqueStats);
 
                 if (mdLinksOptions.validate) {
                     const brokenLinksCount = getBrokenLinksCount(links);
-                    console.log(`Broken: ${brokenLinksCount}`);
+                    const brokenStats = chalk.bgRed.white(`Broken: ${brokenLinksCount}`);
+                    console.log(brokenStats);
                 }
 
             } else if (mdLinksOptions.validate) {
                 links.forEach(link => {
-                    const statusInfo = link.ok ? 'ok' : 'fail';
-                    console.log(colors.bgMagenta(link.file), colors.blueBright(link.href), statusInfo, colors.greenBright(link.status), link.text, `Line: ${link.line}`);
+                    const statusInfo = link.ok ? chalk.green('ok') : chalk.red('fail');
+                    const formattedLink = boxen(
+                        `${chalk.bold.white('File:')} ${chalk.magenta(link.file)}\n` +
+                        `${chalk.bold.white('href:')} ${chalk.blueBright(link.href)}\n` +
+                        `${chalk.bold.white('StatusInfo:')} ${chalk.white(statusInfo)}\n` +
+                        `${chalk.bold.white('Status:')} ${chalk.cyan(link.status)}\n` +
+                        `${chalk.bold.white('Text:')} ${chalk.yellow(link.text)}\n` +  
+                        `${chalk.bold.white('Line:')} ${chalk.gray(link.line)}`,    
+                        {title: 'Validación de Links:', titleAlignment: 'left', padding: 1, margin: 0, borderStyle: 'double' }
+                    );
+                    console.log(formattedLink);
                 });
             } else {
-                // Mostrar enlaces
+                // Mostrar enlaces sin cambios
                 links.forEach(link => {
-                    console.log(`${link.file} ${link.href} ${link.text}, Line: ${link.line}`);
+                    const formattedLink = boxen(
+                        `${chalk.bold.white('File:')} ${chalk.magenta(link.file)}\n` +
+                        `${chalk.bold.white('href:')} ${chalk.blueBright(link.href)}\n` +
+                        `${chalk.bold.white('Text:')} ${chalk.yellow(link.text)}\n` +
+                        `${chalk.bold.white('Line:')} ${chalk.gray(link.line)}`,
+                        {padding: 1, margin: 0, borderStyle: 'double'}
+                    );
+                    console.log(formattedLink);
                 });
             }
         })
         .catch(error => {
-            console.error(colors.red(error.message));
+            console.error(chalk.red(error.message));
         });
 } else {
-    console.error("Por favor, proporciona una ruta válida.");
+    const errorMessage = chalk.red("Por favor, proporciona una ruta válida.");
+    console.error(boxen(errorMessage, { title: 'Error:', titleAlignment: 'left', padding: 1, margin: 0, borderStyle: 'double' })); 
+    yargs.showHelp();
 }
